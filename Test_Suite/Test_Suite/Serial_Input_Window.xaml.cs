678396211712201a07156f6024c90a2ef3ee39fd
Tiago Matias <tiago.matias92@gmail.com>
2018-02-16 08:10:18 +0000

    Script test added... Missing Current Sense only

---
+++ Test_Suite/Test_Suite/Serial_Input_Window.xaml.cs
@@ -0,0 +1,40 @@
+﻿using MahApps.Metro.Controls;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Test_Suite
+{
+    /// <summary>
+    /// Interaction logic for Serial_Input_Window.xaml
+    /// </summary>
+    public partial class Serial_Input_Window : MetroWindow
+    {
+        public string serialNumber = "Cancel";
+        public Serial_Input_Window()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+            Activate();
+            serial_number_txtbox.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            serialNumber = serial_number_txtbox.Text;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            serialNumber = "Cancel";
+            this.Close();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
